a3e2c0dade81c7fc1f441065aa4e995c86bddb24
Priit Kenkmann <priit.kenkmann@khk.ee>
2020-12-19 14:47:36 +0000

    kes on president

---
+++ praks3/yl2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#presiendi valimine
+
+aasta=2016
+president="Kersti Kaljulaid"
+lause_keskosa=". aastal valiti Eesti presidendiks "
+lause=$aasta$lause_keskosa$president
+
+echo $lause
